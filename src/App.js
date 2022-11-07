45a1d46c5dcdf2a7de1dc854dc2fef1c580f4729
DevSatis <sksatishk9@gmail.com>
2022-11-07 15:08:26 +0530

    Ganit-project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import math from "./GanitImages/image6.jpg"
+import {Link} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Ganit is fun, let's play with it..!!!!</h1>
+      <Link to="/Search">Let's Get Started...</Link>
+      <img src={math} alt="This is Math equations."/>
     </div>
   );
 }
---
+++ src/pages/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div>
+        <h1>Let's find what you are looking for..</h1>
+        <h4>At this page you will find the detailed for each and every
+             problem what you are looking for, type you queries to get your answers...</h4>
+
+    </div>
+  )
+}
+
+export default Search
